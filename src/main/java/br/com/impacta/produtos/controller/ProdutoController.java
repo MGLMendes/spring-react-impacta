7c82bf0616547f4daf43483b3d337b33ee716ff7
MGLMendes <miguelmendes203@gmail.com>
2023-10-30 16:26:01 -0300

    feat: Habilitando Cross Origin e adicionando env.cong

--- src/main/java/br/com/impacta/produtos/controller/ProdutoController.java
+++ src/main/java/br/com/impacta/produtos/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import br.com.impacta.produtos.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/produtos")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class ProdutoController {
 
     private final ProdutoService produtoService;
